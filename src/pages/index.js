4f5ccb5d83566dbacf70bc48ce2605e9b1993e9b
phacks <nicolas.goutay@gmail.com>
2018-11-10 14:08:51 +0100

    Use gatsby-source-meetup for Meetup event data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,34 @@
 import React from 'react'
+import { graphql } from "gatsby"
 
 import Layout from '../components/Layout'
 import MeetupWidget from '../components/MeetupWidget/MeetupWidget'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <h1>Le meetup bimensuel autour de la JAMstack</h1>
-    <MeetupWidget meetupEvent={{
-      local_date: '2019-01-23',
-      local_time: "19:00",
-      duration: 10800000,
-      rsvp_limit: 100,
-      yes_rsvp_count: 52,
-      venue: {
-        name: 'Meilleurs Agents',
-        address_1: "7 boulevard Haussmann",
-        city: "Paris",
-      },
-      link: 'https://www.meetup.com/fr-FR/Paris-js/events/251642436/'
-    }}/>
+    <MeetupWidget meetupEvent={data.meetupGroup.childMeetupEvent}/>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    meetupGroup {
+      childMeetupEvent {
+        rsvp_limit
+        local_date
+        local_time
+        yes_rsvp_count
+        venue {
+          name
+          repinned
+          address_1
+          city
+        }
+        link
+      }
+    }
+  }
+`
 
 export default IndexPage
