ccd72c29473c0bb86834c9d2d25d6d1f384d87ff
phacks <nicolas.goutay@gmail.com>
2019-01-29 19:43:37 +0100

    Remove "website" from site name and change favicon

--- src/components/Layout/Layout.component.js
+++ src/components/Layout/Layout.component.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import favicon from '../../images/favicon-16x16.png';
 
 import Header from '../Header'
 import './layout.css'
@@ -22,8 +23,11 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'JAMstack.paris website' },
+            { name: 'description', content: 'JAMstack.paris' },
             { name: 'keywords', content: 'JAMstack, paris' },
+          ]}
+          link={[
+            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
           ]}
         >
           <html lang="en" />
